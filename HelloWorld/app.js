4c6a5c248a759713051fb99b27b776f5186ee32a
jugash.ch <jugash.ch@gmail.com>
2025-01-26 13:12:40 +0000

    Added web server

--- HelloWorld/app.js
+++ HelloWorld/app.js
@@ -1,3 +1,10 @@
-console.log("Hello World!\n");
-const data = 'RE8xMDF4IGlzIGF3ZXNvbWUh';
-console.log(Buffer.from(data, 'base64').toString("ascii"));
+var express = require('express');
+app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello Mars!\n');
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
